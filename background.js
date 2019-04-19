b08408662a2a2fb4782b83c975b3d7dce81b0aae
kenta.hatano <kenta.hatano@s-cubism.jp>
2019-04-19 16:40:00 +0900

    サムネイル画像を圧縮

--- background.js
+++ background.js
@@ -4,16 +4,16 @@ var jpegQuality = 20;
 
 chrome.tabs.onActivated.addListener(function(activatedTabInfo) {
 	chrome.windows.getCurrent(null, function(aWindow) {
-		chrome.tabs.captureVisibleTab(aWindow.id, {format: 'jpeg', quality: jpegQuality}, function(imageUrl) {
-			tabImages[activatedTabInfo.tabId] = new String(imageUrl);
+		chrome.tabs.captureVisibleTab(aWindow.id, {format: 'jpeg'}, function(imageUrl) {
+			tabImages[activatedTabInfo.tabId] = new String(compressImage(imageUrl));
 		});
 	});
 });
 
 chrome.tabs.onUpdated.addListener(function(updatedTabInfo) {
 	chrome.windows.getCurrent(null, function(aWindow) {
-		chrome.tabs.captureVisibleTab(aWindow.id, {format: 'jpeg', quality: jpegQuality}, function(imageUrl) {
-			tabImages[updatedTabInfo.tabId] = new String(imageUrl);
+		chrome.tabs.captureVisibleTab(aWindow.id, {format: 'jpeg'}, function(imageUrl) {
+			tabImages[updatedTabInfo.tabId] = new String(compressImage(imageUrl));
 		});
 	});
 });
@@ -37,10 +37,29 @@ chrome.extension.onMessage.addListener(
 function updateCurrentTab() {
 	chrome.windows.getCurrent(null, function(aWindow) {
 		chrome.tabs.getSelected(aWindow.id, function(selectedTab) {
-			chrome.tabs.captureVisibleTab(aWindow.id, {format: 'jpeg', quality: jpegQuality}, function(imageUrl) {
-				tabImages[selectedTab.id] = new String(imageUrl);
-				chrome.extension.sendMessage("tabImageUpdated", function(response) { });
+			chrome.tabs.captureVisibleTab(aWindow.id, {format: 'jpeg'}, function(imageUrl) {
+				compressImage(imageUrl, function (newImageUrl){
+					tabImages[selectedTab.id] = newImageUrl;
+					chrome.extension.sendMessage("tabImageUpdated", function(response) { });
+				});
 			});
 		});
 	});
+}
+
+function compressImage(imageUrl, callback) {
+	var originalImage = new Image();
+	originalImage.src = imageUrl;
+
+	originalImage.onload = function() {
+		var imageSize = Math.min(originalImage.width, originalImage.height);
+		var newCanvas = document.createElement('canvas');
+		newCanvas.width = 176;
+		newCanvas.height = 150;
+		var ctx = newCanvas.getContext('2d');
+		ctx.drawImage(originalImage, 0, 0, imageSize, imageSize,
+			0, 0, 176, 176);
+
+		callback(newCanvas.toDataURL('image/jpeg', jpegQuality));
+	};
 }
